74b2829810ca3b9e05bb2237e34454fa0e0a2908
EdgarsBe <edgarsberzins100@gmail.com>
2023-03-09 21:29:01 +0200

    Version 1 is done

--- Assets/Scripts/ObjectiveReached.cs
+++ Assets/Scripts/ObjectiveReached.cs
@@ -6,11 +6,12 @@ using UnityEngine.SceneManagement;
 public class ObjectiveReached : MonoBehaviour
 {
     public GameObject WinMenu;
-
+    [SerializeField] PlayerMovement Move;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            Move.canMove = false;
             Time.timeScale = 0f;
             WinMenu.SetActive(true);
         }
